ae8eb33db2833cf92a95d8e79ca7bab74b4e6d5b
StevenW <stevenwisniewski@gmail.com>
2015-07-25 22:55:36 -0400

    react-youtube-preview.js - switched to es6 export

--- react-youtube-preview.js
+++ react-youtube-preview.js
@@ -1,53 +1,51 @@
-(function() {
-	'use strict';
+'use strict';
 
-			var YouTubePreview = React.createClass({
-				getInitialState: function () {
-					return {
-						showVideo: false
-					};
-				},
-				getDefaultProps: function () {
-					return {
-						frameKey: 0
-					};
-				},
-				render: function () {
-					var mainStyle = {
-						width: this.props.width,
-						height: this.props.height,
-						cursor: "pointer"
-					};
+export var YouTubePreview = React.createClass({
+	getInitialState: function () {
+		return {
+			showVideo: false
+		};
+	},
+	getDefaultProps: function () {
+		return {
+			frameKey: 0
+		};
+	},
+	render: function () {
+		var mainStyle = {
+			width: this.props.width,
+			height: this.props.height,
+			cursor: "pointer"
+		};
 
-					return (
-						<div style={mainStyle} onClick={this.play}>
-							{this.state.showVideo === false ? <YouTubeImage v={this.props.v} frameKey={this.props.frameKey}></YouTubeImage> : null}
-							{this.state.showVideo === true ? <YouTubeEmbed v={this.props.v}></YouTubeEmbed> : null}
-						</div>
-					);
-				},
-				play: function () {
-					this.setState({showVideo: true});
-				}
-			});
+		return (
+			<div style={mainStyle} onClick={this.play}>
+				{this.state.showVideo === false ? <YouTubeImage v={this.props.v} frameKey={this.props.frameKey}></YouTubeImage> : null}
+				{this.state.showVideo === true ? <YouTubeEmbed v={this.props.v}></YouTubeEmbed> : null}
+			</div>
+		);
+	},
+	play: function () {
+		this.setState({showVideo: true});
+	}
+});
 
-			var YouTubeImage = React.createClass({
-				getPreviewURL: function () {
-					return "http://img.youtube.com/vi/" + this.props.v + "/" + this.props.frameKey + ".jpg";
-				},
-				render: function () {
-					return <img src={this.getPreviewURL()} alt="YouTube preview" />
-				}
-			});
+var YouTubeImage = React.createClass({
+	getPreviewURL: function () {
+		return "http://img.youtube.com/vi/" + this.props.v + "/" + this.props.frameKey + ".jpg";
+	},
+	render: function () {
+		return <img src={this.getPreviewURL()} alt="YouTube preview" />
+	}
+});
 
-			var YouTubeEmbed = React.createClass({
-				getVideoURL: function () {
-					return "http:////www.youtube.com/embed/" + this.props.v + "?rel=0&amp;autoplay=1";
-				},
-				render: function () {
-					return (
-						<iframe src={this.getVideoURL()} width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
-					);
-				}
-			});
-})();
+var YouTubeEmbed = React.createClass({
+	getVideoURL: function () {
+		return "http:////www.youtube.com/embed/" + this.props.v + "?rel=0&amp;autoplay=1";
+	},
+	render: function () {
+		return (
+			<iframe src={this.getVideoURL()} width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
+		);
+	}
+});
